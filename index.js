6c3b778f7863f6adc5fce12bd10982460a7c4643
Srikanth Arya <srikantharya1@gmail.com>
2024-03-14 20:08:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const heading = React.createElement("div", { id: "parent" }, 
+[React.createElement("div", { id: "child" }, [
+React.createElement("h1", { id: "none" }, "Create react Element"),
+React.createElement("h2", { id: "none2" }, "Create react Element2")
+]),
+React.createElement("div", { id: "child2" }, [
+React.createElement("h1", { id: "none" }, "Create react Element"),
+React.createElement("h2", { id: "none2" }, "Create react Element2")
+])
+]);
+
+console.log(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
